670189a5db06174b036c3c3b71590b15d56fd62a
dhkaneda <ryqz.ig@gmail.com>
2020-04-17 16:53:56 -0700

    Dev port change

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 5000;
+const port = 4444;
 
 app.listen(port, () => {
   console.log(`Server is running, listening on port ${port}`);
