7bdeee970f65986ad263ddc8a7340a431aee662c
dhkaneda <ryqz.ig@gmail.com>
2020-04-18 10:44:56 -0700

    Create App component and change anchor

---
+++ client/src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        App Test
+      </div>
+    );
+  }
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 import './styles/normalize.css';
 import './styles/skeleton.css';
 
+
 ReactDOM.render(
-  <div>
-    ReactWORKS
-  </div>,
-  document.getElementById('app'),
+  <App />,
+  document.getElementById('root'),
 );
