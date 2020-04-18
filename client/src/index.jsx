062154e31e50f5069de497372966da57e07fc9fa
dhkaneda <ryqz.ig@gmail.com>
2020-04-18 10:53:44 -0700

    Setup React Routing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import App from './components/App';
 import './styles/normalize.css';
 import './styles/skeleton.css';
 
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/App">App</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={Home} />
+      <Route path="/app" component={App} />
+    </div>
+  </Router>
+);
 
 ReactDOM.render(
-  <App />,
+  routing,
   document.getElementById('root'),
 );
--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.post('/api/users', createUser);
 app.post('/api/contracts', createContract);
 app.get('/api/users', getUser);
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
