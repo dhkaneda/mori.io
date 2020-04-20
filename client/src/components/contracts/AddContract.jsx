d87177a0bde843391cb0a75550584ac4d11759b1
dhkaneda <ryqz.ig@gmail.com>
2020-04-20 09:53:19 -0700

    Conditionally render collateral selections

--- client/src/components/contracts/AddContract.jsx
+++ client/src/components/contracts/AddContract.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddContract = ({
-  walkthroughFinish,
-  handleInputChange,
-  handleContractSubmit,
-  description,
-  collateral,
-  service,
-  amount,
-}) => {
-  return (
-    <div>
-      <form onSubmit={walkthroughFinish || handleContractSubmit}>
-        <input
-          type="text"
-          name="description"
-          defaultValue={description}
-          onChange={handleInputChange}
-        />
-        <br />
+class AddContract extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStep: 0,
+    };
+
+    this.handleNext = this.handleNext.bind(this);
+  }
+
+  handleNext() {
+    let { currentStep } = this.state;
+    this.setState({ currentStep: currentStep += 1 });
+  }
+
+  render() {
+    const { currentStep } = this.state;
+    const {
+      walkthroughFinish,
+      handleInputChange,
+      handleContractSubmit,
+      description,
+      collateral,
+      // service,
+      // amount,
+    } = this.props;
+
+    let options;
+    let select;
+
+    if (currentStep === 1) {
+      select = (
         <select
           name="collateral"
           id="collateral"
@@ -30,25 +43,42 @@ const AddContract = ({
           <option value="digital">My avatar</option>
           <option value="life">My life</option>
         </select>
-        <br />
-        {/* <input
-          type="text"
-          name="service"
-          defaultValue={service}
-          onChange={handleInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="amount"
-          defaultValue={amount}
-          onChange={handleInputChange}
-        />
-        <br /> */}
-        <input type="submit" value="test" />
-      </form>
-    </div>
-  );
-};
+      );
+    } else {
+      select = <button type="button" onClick={this.handleNext}>I&apos;m sure</button>;
+    }
+
+    switch (collateral) {
+      case 'vice':
+        break;
+      case 'monetary':
+        break;
+      case 'digital':
+        break;
+      case 'life':
+        break;
+      default:
+        options = null;
+    }
+
+    return (
+      <div>
+        <form onSubmit={walkthroughFinish || handleContractSubmit}>
+          <input
+            type="text"
+            name="description"
+            defaultValue={description}
+            onChange={handleInputChange}
+          />
+          <br />
+          {select}
+          <br />
+          {options}
+          <input type="submit" value="submit contract" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddContract;
