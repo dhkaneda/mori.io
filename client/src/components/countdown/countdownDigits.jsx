986c402cf04d6453efc717867b1e9f7b85a1bdf3
dhkaneda <ryqz.ig@gmail.com>
2020-07-05 20:58:25 -0700

    Start countdown refactor

--- client/src/components/countdown/countdownDigits.jsx
+++ client/src/components/countdown/countdownDigits.jsx
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 const CountdownDigitsContainer = ({ targetDate }) => {
   const [secondsLeft, setSecondsLeft] = useState('loading');
 
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
   const findSeconds = () => {
     const now = new Date();
     const secondsNow = Math.floor((targetDate - now.getTime()) / 1000);
@@ -10,9 +14,46 @@ const CountdownDigitsContainer = ({ targetDate }) => {
     if (secondsNow === 0) {
       alert('OVER');
     } else {
-      return secondsNow;
+      return numberWithCommas(secondsNow);
     }
   };
+
+  const findYears = (milliseconds) => {
+    const now = new Date();
+    const yearsNow = Math.floor((milliseconds - now.getTime()) / 31556952000);
+    return yearsNow;
+  };
+
+  const findMonths = (milliseconds) => {
+    const now = new Date();
+    const monthsNow = Math.floor((milliseconds - now.getTime()) / 2629756000);
+    return monthsNow;
+  };
+
+  const findWeeks = (milliseconds) => {
+    const now = new Date();
+    const weeksNow = Math.floor((milliseconds - now.getTime()) / 604800000);
+    return weeksNow;
+  };
+
+  const findDays = (milliseconds) => {
+    const now = new Date();
+    const daysNow = Math.floor((milliseconds - now.getTime()) / 86400000);
+    return daysNow;
+  };
+
+  const findHours = (milliseconds) => {
+    const now = new Date();
+    const hoursNow = Math.floor((milliseconds - now.getTime()) / 3600000);
+    return hoursNow;
+  };
+
+  const findMinutes = (milliseconds) => {
+    const now = new Date();
+    const minutesNow = Math.floor((milliseconds - now.getTime()) / 60000);
+    return minutesNow;
+  };
+
 
   // starts countdown automatically
   useEffect(() => {
