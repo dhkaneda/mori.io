c8ddb33f58c5285181072aed2a2c112a84efdeb6
dhkaneda <ryqz.ig@gmail.com>
2020-08-12 11:28:05 -0700

    Remove db dependency

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('../database');
+// require('../database');
 
 const app = express();
 const port = 4444;
